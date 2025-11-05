8fcde785a28c0db1eff8f4d50dccd99da13212b0
elhan414 <elhanattokurov@gmail.com>
2025-11-05 19:57:13 +0600

    fdfdfdgf

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from flask import Flask
 
 aap = Flask(__name__)
 @aap.route('/')
-
 def hello():
     return 'Hello World!'
 
-if __name__ == '__name__':
+@aap.route("/about_my")
+def about_my():
+    return "My name is Elhan"
+
+@aap.route("/Elhan")
+def Elhan():
+    return "Я сегодня встретил Элхана. Он выглядел очень довольным.Элхан обещал помочь с этим проектом. Надеюсь, у него получится.Мы обсуждали последние новости с Элханом. Интересный разговор получился."
+
+if __name__ == '__main__':
     aap.run(debug=True)
