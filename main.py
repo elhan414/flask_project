08d81e768cf7ba7764f14eb359e98e07614ee40e
elhan414 <elhanattokurov@gmail.com>
2025-11-05 19:15:05 +0600

    fdfdfd

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("gdfgdfgdgdfg")
+from flask import Flask
+
+
+aap = Flask(__name__)
+@aap.route('/')
+
+def hello():
+    return 'Hello World!'
+
+if __name__ == '__name__':
+    aap.run(debug=True)
